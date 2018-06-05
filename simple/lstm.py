a499cb400169064b2674af2f3b52eaa821aef21b
Thomas Miconi <tmiconi@uber.com>
2018-06-04 17:16:45 -0700

    .

--- simple/lstm.py
+++ simple/lstm.py
@@ -235,7 +235,7 @@ for numiter in range(1000000):
         all_losses.append(total_loss)
         print("Mean loss over last", print_every, "iters:", total_loss)
         print("")
-        fname = 'loss_binary_lstm_nbhneur_'+str(NBHIDDENNEUR)+'_lr_'+str(ADAMLEARNINGRATE)+'_prestime_'+str(PRESTIME)+'_interpresdelay_'+str(INTERPRESDELAY)+'_rngseed_'+str(RNGSEED)+'.txt'
+        fname = 'loss_binary_lstm_nbhneur_'+str(NBHIDDENNEUR)+'_clamp_'+str(CLAMPING)+'_lr_'+str(ADAMLEARNINGRATE)+'_prestime_'+str(PRESTIME)+'_interpresdelay_'+str(INTERPRESDELAY)+'_rngseed_'+str(RNGSEED)+'.txt'
         with open(fname, 'w') as fo:
             for item in all_losses:
                 fo.write("%s\n" % item)
